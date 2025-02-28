923a6fb1d3ac6c4459e3568d6b6160a05655e6b3
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-28 17:29:56 +0600

    enhance wrist size selection functionality with visual feedback on button states

--- js/script.js
+++ js/script.js
@@ -14,10 +14,17 @@ for (let i = 0; i < ringButton.length; i++) {
     });
 }
 
-
 function selectWristSize(size) {
-    console.log(size);
-    
+    const sizes = ['S', 'M', 'L', 'XL'];
+    for (let i = 0; i < sizes.length; i++) {
+        const sizeBtn = document.getElementById('size-' + sizes[i]);
+        const element = sizes[i];
+        if (element === size) {
+            sizeBtn.classList.add('border-purple-500'); 
+        } else {
+            sizeBtn.classList.remove('border-purple-500');
+        }
+    }
 }
 
 
