5f46d563dc04b66957dd46637c88d11f001c842c
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-28 17:08:25 +0600

    add wrist size selection functionality and improve button event handling on smartwatch product page

--- js/script.js
+++ js/script.js
@@ -2,15 +2,24 @@ const ringButton = document.querySelectorAll(".ring-button");
 for (let i = 0; i < ringButton.length; i++) {
     const ringBtn = ringButton[i];
     ringBtn.addEventListener("click", function (event) {
-        const color = event.target.id.split("-")[0]; 
-        
+        const color = event.target.id.split("-")[0];
+
         for (let j = 0; j < ringButton.length; j++) {
             ringButton[j].classList.remove("border-purple-500");
         }
         event.target.classList.add("border-purple-500");
 
         const productImage = document.getElementById("product-image");
-        productImage.src = "./images/"+color+".png";
+        productImage.src = "./images/" + color + ".png";
     });
-
 }
+
+
+function selectWristSize(size) {
+    console.log(size);
+    
+}
+
+
+
+
