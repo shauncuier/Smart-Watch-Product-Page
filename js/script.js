026237b8a33d8116c30b46519c701fc8ba9f145b
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-02-28 16:24:49 +0600

    update button styles and add event listeners for color selection on smartwatch product page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-
+const ringButton = document.querySelectorAll(".ring-button");
+for (let i = 0; i < ringButton.length; i++) {
+    const ringBtn = ringButton[i];
+    ringBtn.addEventListener("click", function (event) {
+        for (let j = 0; j < ringButton.length; j++) {
+            ringButton[j].classList.remove("border-purple-500");
+        }
+        event.target.classList.add("border-purple-500");
+    });
+}
